43c792d60274e044165fcc419c07e0776f1a778c
ISU <ourwavelets@gmail.com>
2025-01-14 23:16:10 +0900

    add: GeneralException 코드 추가

---
+++ src/main/java/com/example/everguide/api/exception/GeneralException.java
@@ -0,0 +1,21 @@
+package com.example.everguide.api.exception;
+
+import com.example.everguide.api.code.BaseErrorCode;
+import com.example.everguide.api.code.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+    private BaseErrorCode code;
+
+    public ErrorReasonDTO getErrorReason() {
+        return this.code.getReason();
+    }
+
+    public ErrorReasonDTO getErrorReasonHttpStatus() {
+        return this.code.getReasonHttpStatus();
+    }
+}
