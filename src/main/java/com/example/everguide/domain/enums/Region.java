1decf47bc0cdf53cc7e9d7d633cdc2cff414a586
ISU <wavesinu@gmail.com>
2025-01-31 01:03:57 +0900

    refactor: Region 수정
    - fromString() 메서드 추가

--- src/main/java/com/example/everguide/domain/enums/Region.java
+++ src/main/java/com/example/everguide/domain/enums/Region.java
@@ -1,16 +1,37 @@
 package com.example.everguide.domain.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Region {
-    SEOUL,
-    GYEONGGI,
-    INCHEON,
-    BUSAN,
-    CHUNGBUK,
-    CHUNGNAM,
-    JEONBUK,
-    JEONNAM,
-    GYEONGBUK,
-    GYEONGNAM,
-    GANGWON,
-    JEJU
+    SEOUL("서울특별시"),
+    BUSAN("부산광역시"),
+    DAEGU("대구광역시"),
+    INCHEON("인천광역시"),
+    GWANGJU("광주광역시"),
+    DAEJEON("대전광역시"),
+    ULSAN("울산광역시"),
+    SEJONG("세종특별자치시"),
+    GYEONGGI("경기도"),
+    GANGWON("강원도"),
+    CHUNGBUK("충청북도"),
+    CHUNGNAM("충청남도"),
+    JEONBUK("전라북도"),
+    JEONNAM("전라남도"),
+    GYEONGBUK("경상북도"),
+    GYEONGNAM("경상남도"),
+    JEJU("제주특별자치도");
+
+    private final String description;
+
+    public static Region fromString(String text) {
+        for (Region region : Region.values()) {
+            if (region.getDescription().equals(text)) {
+                return region;
+            }
+        }
+        return SEOUL; // 기본값 설정
+    }
 }
