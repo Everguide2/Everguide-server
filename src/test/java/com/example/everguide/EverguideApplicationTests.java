81c7917a11db48fa72417e87644fe3256f2a3ef7
chang min O <82094699+Ochangmin524@users.noreply.github.com>
2025-02-15 23:18:56 +0900

    chore: welfdata 일시 주석 및 테스트 주석 삭제

--- src/test/java/com/example/everguide/EverguideApplicationTests.java
+++ src/test/java/com/example/everguide/EverguideApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.example.everguide;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class EverguideApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//
-//    }
-//}
+package com.example.everguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverguideApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
