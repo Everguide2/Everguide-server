ac94ff7f2a5b6991935a40f6c21a94be74b76a63
ISU <wavesinu@gmail.com>
2025-02-17 17:43:22 +0900

    feat: 알림 도메인 및 타입 enum 추가

--- src/main/java/com/example/everguide/domain/Notification.java
+++ src/main/java/com/example/everguide/domain/Notification.java
@@ -7,6 +7,8 @@ import lombok.*;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Builder
@@ -21,12 +23,28 @@ public class Notification extends BaseEntity {
     @Column(nullable = false, unique = true)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private NotifyType type;
 
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = true)
+    private String resourceUrl;
+
     @Column(nullable = false, columnDefinition = "BIT DEFAULT 0")
     private Boolean isRead;
+
+    @Column(nullable = true)
+    private LocalDateTime expirationDate;
 
     public void markAsRead() {
         this.isRead = true;
