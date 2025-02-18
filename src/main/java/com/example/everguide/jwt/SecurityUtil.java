022b4de64afb2a9b66e8b0ecf09271c5ebe66104
hekim52 <acrossnightsky@gmail.com>
2025-02-18 20:49:34 +0900

    refactor: SecurityUtil 수정 #25

--- src/main/java/com/example/everguide/jwt/SecurityUtil.java
+++ src/main/java/com/example/everguide/jwt/SecurityUtil.java
@@ -3,6 +3,7 @@ package com.example.everguide.jwt;
 import com.example.everguide.api.exception.MemberBadRequestException;
 import com.example.everguide.web.dto.auth.CustomOAuth2User;
 import com.example.everguide.web.dto.auth.CustomUserDetails;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -15,21 +16,21 @@ public class SecurityUtil {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String currentUserId;
 
-        if (authentication == null || authentication.getPrincipal().equals("anonymousUser")) {
-            throw new MemberBadRequestException("로그인되지 않았습니다.");
+        if (authentication == null || !authentication.isAuthenticated() || !(authentication instanceof AnonymousAuthenticationToken)) {
+            throw new MemberBadRequestException("인증 사용자가 아닙니다.");
+
+        }
+
+        if (authentication.getPrincipal().getClass().getSimpleName().equals("CustomUserDetails")) {
+            CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+            currentUserId = customUserDetails.getUsername();
+
+        } else if (authentication.getPrincipal().getClass().getSimpleName().equals("CustomOAuth2User")) {
+            CustomOAuth2User customOAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+            currentUserId = customOAuth2User.getUserId();
 
         } else {
-            if (authentication.getPrincipal().getClass().getSimpleName().equals("CustomUserDetails")) {
-                CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-                currentUserId = customUserDetails.getUsername();
-
-            } else if (authentication.getPrincipal().getClass().getSimpleName().equals("CustomOAuth2User")) {
-                CustomOAuth2User customOAuth2User = (CustomOAuth2User) authentication.getPrincipal();
-                currentUserId = customOAuth2User.getUserId();
-
-            } else {
-                throw new MemberBadRequestException("authentication.getPrincipal()의 타입이 올바르지 않음");
-            }
+            throw new MemberBadRequestException("authentication.getPrincipal()의 타입이 올바르지 않음");
         }
 
         return currentUserId;
